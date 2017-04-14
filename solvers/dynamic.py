68ac8afaa8432cea1fbbeef00b05e7edb6958b30
Angus Hammond <angusgh@gmail.com>
2017-04-14 15:29:03 +0100

    Remove dead code from dynamic programming solver

--- solvers/dynamic.py
+++ solvers/dynamic.py
@@ -1,19 +1,5 @@
 import itertools
 import math
-# Use itertools.combinations instead
-def combinations(items, n):
-    if n==0:
-        yield set()
-        return
-    if n > len(items):
-        return
-    
-    x = {items[0]}
-    items = items[1:]
-
-    for comb in combinations(items, n-1):
-        yield x | comb
-    yield from combinations(items, n)
 
 def dist(a, b):
     return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
